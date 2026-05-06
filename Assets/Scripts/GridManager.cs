87ce4ed4dbdbeff924c71f748841d91fbac82e10
Prem Agarwal <prem.m.agarwal@gmail.com>
2026-05-06 13:35:31 -0700

    added color changes to grid tiles green and different shade of green

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -18,10 +18,11 @@ public class GridManager : MonoBehaviour
         {
             for (int y = 0; y < _height; y++)
             {
-                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity) ;
                 spawnedTile.name = $"Tile {x} {y}";
 
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                spawnedTile.Init(isOffset);
             }
         }
         _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
