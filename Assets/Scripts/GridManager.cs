6ec3c692fcc6d8a183ebc7be2c50f43041ccbfd5
Prem Agarwal <prem.m.agarwal@gmail.com>
2026-05-28 11:13:36 -0700

    updated and working dictionary and new csharp scripts for tiles

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
@@ -13,31 +16,32 @@ public class GridManager : MonoBehaviour
     void Start()
     {
         GenerateGrid();
-    }   
+    }
+
     void GenerateGrid()
     {
+        _tiles = new Dictionary<Vector2, Tile>();
         for (int x = 0; x < _width; x++)
         {
             for (int y = 0; y < _height; y++)
             {
-                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity) ;
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
 
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
                 spawnedTile.Init(isOffset);
 
 
-                _tiles[new Vector2(x, y)] = spawnedTile
+                _tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
+
         _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
     }
-    public Tile GetTileAtPosition(Vector2 position)
+
+    public Tile GetTileAtPosition(Vector2 pos)
     {
-        if (_tiles.TryGetValue(position, out var tile))
-        {
-            return tile;
-        }
+        if (_tiles.TryGetValue(pos, out var tile)) return tile;
         return null;
     }
 }
