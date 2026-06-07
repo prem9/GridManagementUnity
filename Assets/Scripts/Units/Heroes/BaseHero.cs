57722785d1dcf3d2aaf3d41b51103c969b4211ed
Prem Agarwal <prem.m.agarwal@gmail.com>
2026-06-06 18:06:26 -0700

    add more files

---
+++ Assets/Scripts/Units/Heroes/BaseHero.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHero : BaseUnit
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
