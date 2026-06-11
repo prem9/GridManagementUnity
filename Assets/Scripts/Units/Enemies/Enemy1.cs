2ccbb5ba3276e33937b60232cc75ce3d1fe7393f
Prem Agarwal <prem.m.agarwal@gmail.com>
2026-06-11 13:30:07 -0700

    some file changes and new folders

--- Assets/Scripts/Units/Enemies/Enemy1.cs
+++ Assets/Scripts/Units/Enemies/Enemy1.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Enemy1 : MonoBehaviour
+public class Enemy1 : BaseEnemy
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
